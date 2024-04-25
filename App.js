e610fb5843d958241127f2272423de65c38424d4
tanishqa1508 <96232293+tanishqa1508@users.noreply.github.com>
2024-04-25 23:40:00 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import { StatusBar } from 'expo-status-bar';
+
+
+//screens
+import Login from './screens/Login';
+import Signup from './screens/Signup';
+import Welcome from './screens/Welcome';
+
+export default function App() {
+  return (
+    <Login/>
+  );
+}
+
+
